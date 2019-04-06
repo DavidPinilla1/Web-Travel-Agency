81890ff9cf1d8340ac678f1547fd3fabd933482b
DavidPinilla1 <metroid_300@hotmail.com>
2019-04-06 21:21:19 +0200

    multer configuration added

---
+++ server/config/multer.js
@@ -0,0 +1,23 @@
+const Multer = require( 'multer' );
+
+const multer = Multer(  {
+    storage: Multer.diskStorage( {
+        destination: ( req, file, callback ) => {
+            callback( null, './uploads/' );
+        },
+        filename: ( req, file, callback ) => {
+            callback( null, file.originalname );
+        },
+    }),
+    fileFilter: ( req, file, callback ) => {
+
+        if ( file.mimetype === 'image/png' || file.mimetype === 'image/jpg'||file.mimetype === 'image/jpeg' ) {
+            callback(null , true)
+        } else {
+            callback( null, false )
+        }
+    },
+    limits: {fileSize: 2*1024*1024}
+});
+
+module.exports = multer;
