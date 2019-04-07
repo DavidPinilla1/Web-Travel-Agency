b5b9eb8fddd2d9e99c7d6a5488ba46a1e5d136c0
DavidPinilla1 <metroid_300@hotmail.com>
2019-04-07 12:38:19 +0200

    added flash message to not authorized personal who attempt to access private sections

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,8 +1,11 @@
 const auth=  ()=> {  
 	return (req, res, next) => {
 		console.log(`req.session.passport.user: ${JSON.stringify(req.session.passport)}`);
-	    if (req.isAuthenticated()) return next();
-	    res.redirect('/login')
+        if (req.isAuthenticated()) return next();
+        else{
+            req.flash('warn', 'You should be logged in to access this section')
+            res.redirect('/login')
+        }
 	}
 }
 module.exports=auth;
