af24fe491e8acaad64022f52ec3918c68e4d707f
DavidPinilla1 <metroid_300@hotmail.com>
2019-04-07 09:39:43 +0200

    added authenticate middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,8 @@
+const auth=  ()=> {  
+	return (req, res, next) => {
+		console.log(`req.session.passport.user: ${JSON.stringify(req.session.passport)}`);
+	    if (req.isAuthenticated()) return next();
+	    res.redirect('/login')
+	}
+}
+module.exports=auth;
