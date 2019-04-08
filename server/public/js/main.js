efa3b5cf0f8679b2881f73ef293757df30abf34e
DavidPinilla1 <metroid_300@hotmail.com>
2019-04-08 10:16:33 +0200

    added javascript event listeners to click the hide image input

--- server/public/js/main.js
+++ server/public/js/main.js
@@ -6,16 +6,20 @@ function check(input) {
     }
 }
 document.addEventListener('DOMContentLoaded', () => {
-    const password = document.querySelector('.password');
+    const password = document.querySelector('.register .password');
+    const passwordConfirm = document.querySelector('.passwordConfirm');
+    const profileImage = document.querySelector('.profileImage .edit');
     if (password) password.addEventListener('keyup', ev => {
-            let warning = document.querySelector('.passwordRule');
-            if (ev.target.value.length >= 8) warning.style.display = 'none'
-            else if (ev.target.value.length < 8) warning.style.display = 'block'
-        })
-     if(document.querySelector('.passwordConfirm')) document.querySelector('.passwordConfirm').addEventListener('keyup', ev => {
-            let warning = document.querySelector('.passwordRule2');
+        let warning = document.querySelector('.passwordRule');
+        if (ev.target.value.length >= 8 || ev.target.value.length == 0) warning.style.display = 'none'
+        else if (ev.target.value.length < 8) warning.style.display = 'block'
+    })
+    if (passwordConfirm) passwordConfirm.addEventListener('keyup', ev => {
+        let warning = document.querySelector('.passwordRule2');
             if (ev.target.value === password.value) warning.style.display = 'none'
             if (ev.target.value !== password.value) warning.style.display = 'block'
-        })
-    
+    })
+    if (profileImage) profileImage.addEventListener('click', ev => {
+        document.querySelector('.hiddenInput').click()
+    })
 })
