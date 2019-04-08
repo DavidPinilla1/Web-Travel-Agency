c70e719d5482d7249bccc84697be0cf9c28decea
DavidPinilla1 <metroid_300@hotmail.com>
2019-04-08 10:14:24 +0200

    update routes completed

---
+++ server/routes/user.js
@@ -0,0 +1,90 @@
+const router = require('express').Router();
+const config = require('../config/password')
+const User = require('../models/User');
+const transporter = require('../config/nodemailer');
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt');
+const passport = require('passport')
+const upload=require('../config/multer')
+
+router.post('/signup', (req, res) => {
+    jwt.sign({
+        user: req.body.email
+    }, config.EMAIL_SECRET, {
+        expiresIn: '2d'
+    }, (err, emailToken) => {
+        if (err) return res.send(err)
+        const url = `http://localhost:3001/user/confirmation/${emailToken}`
+        transporter.sendMail({
+            to: req.body.email,
+            subject: 'Confirm your email in Travel Agency',
+            html: `<h3>Please click the following link to confirm your email:<br>
+                <a href="${url}">Click here to confirm your email</a><br>
+                    The link above will expire in 48 hours.</h3> `
+        }).then(console.log).catch(console.log)
+    })
+    new User({
+        ...req.body,
+        imagePath:'/images/profile.png'
+    }).save().then(user =>{
+        req.flash('info', 'user succesfully created, please confirm your email ');
+         res.redirect('/login')
+    }).catch(err =>{
+        req.flash('error', 'Email already in use, please choose another email');
+        res.redirect('/register')
+    })
+});
+router.get('/confirmation/:token', (req, res) => {
+    const email = jwt.verify(req.params.token, config.EMAIL_SECRET).user;
+    User.findOne({ email }).then(userFound => {
+        userFound.confirmed = true
+        userFound.save().then(user => {
+            req.login(userFound._id, err => {
+                    req.flash('info', 'Congratulations! Your email account has been verified.');
+                    res.redirect('/')
+            })
+        }).catch(console.log)
+    }).catch(console.log)
+})
+router.post('/login', (req, res) => {
+    User.findOne({
+        email: req.body.email
+    }).then(userFound => {
+        if (!userFound) {
+            req.flash('error','Email or password wrong');
+            return res.redirect('/login');
+        }
+        if(!userFound.confirmed) {
+            req.flash('warn','You should confirm your email to log in');
+            return  res.redirect('/login');
+        }
+        bcrypt.compare(req.body.password, userFound.password).then(isMatch => {
+            if (!isMatch){
+                req.flash('error','Email or password wrong');
+               return res.redirect('/login');
+            }
+            req.login(userFound._id, err => {
+                res.redirect('/');
+            })
+        }).catch(err => res.send(err))
+    })
+});
+router.post('/update',upload.single('image'),(req,res)=>{
+     User.findOneAndUpdate(req.session.passport.user,{
+        ...req.body,
+        imagePath: '/images/users/'+req.file.filename
+     }).then(user=>{
+        req.flash('info', 'User successfully updated')
+        res.redirect('/profile')
+     }).catch(err=>console.log(err))
+})
+passport.serializeUser(function (user_id, done) {
+    done(null, user_id);
+});
+
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+module.exports = router;
